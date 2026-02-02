f25892e0b806f2276e5435c175e38c00a8bd6210
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-02-02 15:55:56 +0530

    day 1 intro and project installation

---
+++ intro/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar"
+function App(){
+  let a=10
+  return (
+    <div>
+      <Navbar />
+      <About />
+      <Contact />
+      <Services />
+    {/* <h2>this is first class in 70_72r react day 1 intro class </h2>
+    <h1>{a}</h1> */}
+  </div>
+  )
+}
+export default App
+
+// comp :--comp is nothing but js function which return jsx
+// jsx :-- jsx is nothing writing js inside html
---
+++ intro/src/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./navbar.css"
+function Navbar(){
+ return (
+    <div id="navbar">
+        <h2>10000Coders</h2>
+        <div id="nav_Links">
+            <span>Home</span>
+            <span>About</span>
+            <span>Contact</span>
+            <span>Services</span>
+        </div>
+    </div>
+ )
+}
+
+export default Navbar
